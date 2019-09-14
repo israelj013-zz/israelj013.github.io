ae20b5b67669e3339ce8e684a53eb9b34c47ad95
israeljimenez <israelj@uci.edu>
2019-09-13 22:10:19 -0700

    home text changes

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const text = document.getElementsByClassName('border');
+
+
+function resize(){
+    if(window.innerWidth < 410){
+        text[0].innerHTML = `Computer Science and Engineering`
+    }
+    else if (window.innerWidth < 860){
+        text[0].innerHTML = `B.S in Computer Science and Engineering`;
+    }
+    else{
+        text[0].innerHTML = `B.S in Computer Science and Engineering | University of California, Irvine`
+    }
+}
+
+resize()
+window.onresize =  resize
